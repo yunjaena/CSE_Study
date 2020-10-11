8a1f3263af34c7e0a36bb9ff537c5fe4b16870c1
Yunjae-Na <yunjaena@gmail.com>
2020-10-11 21:12:53 +0900

    Add BOJ1644
    Add BOJ1644

---
+++ algorithm/src/boj/BOJ1644.kt
@@ -0,0 +1,67 @@
+package boj
+
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+fun main(): Unit = with(BufferedReader(InputStreamReader(System.`in`))) {
+    val primNumberList = arrayListOf<Int>()
+    val N = readLine().toInt()
+    initPrime(primNumberList, N)
+    println(count(primNumberList, N))
+}
+
+fun initPrime(primeNumberList: ArrayList<Int>, limit: Int) {
+    if (limit < 2) {
+        return
+    }
+
+    val isPrimeArray = Array(limit + 1) { it >= 2}
+
+    var i = 2
+
+    while (true) {
+        if (i * i >  limit) {
+            break
+        }
+
+        if (isPrimeArray[i]) {
+            var j = i * i
+            while (true) {
+                if (j > limit) {
+                    break
+                }
+                isPrimeArray[j] = false
+                j += i
+            }
+        }
+        i++
+    }
+
+    isPrimeArray.forEachIndexed { index, isPrime ->
+        if (isPrime) {
+            primeNumberList.add(index)
+        }
+    }
+}
+
+fun count(primeNumberList: ArrayList<Int>, limit: Int): Int {
+    var count = 0
+    var isEnd = false
+    var index = 0
+    if (primeNumberList.isEmpty())
+        return 0
+    while (!isEnd) {
+        if (primeNumberList[0] > limit || index >= primeNumberList.size)
+            isEnd = true
+        var sum = 0
+        for (i in index until primeNumberList.size) {
+            sum += primeNumberList[i]
+            if (sum >= limit) {
+                if (sum == limit) count++
+                break
+            }
+        }
+        index++
+    }
+    return count
+}
